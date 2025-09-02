b35f88ef03a6bf6990a4ab113a69f192422785e8
Margaret Nieves Gonzales <138099922+MARGYYY1231@users.noreply.github.com>
2025-09-02 00:19:43 +0000

    Added function to hash password

--- password_cracker.py
+++ password_cracker.py
@@ -1,4 +1,9 @@
 import hashlib
 
-def crack_sha1_hash(hash, use_salts = False):
-    return True
+
+def hash_with_sha1(password):
+  return hashlib.sha1(password.encode('utf-8')).hexdigest()
+
+
+def crack_sha1_hash(hash, use_salts=False):
+  return True
